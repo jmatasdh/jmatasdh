5f602c1f2f3c363242dfdc883509eea85109fef6
jmatasdh <jmatasdh@gmail.com>
2016-08-02 21:27:34 +0200

    Menu Bar

--- js/main.js
+++ js/main.js
@@ -5,11 +5,20 @@ var tips = [
 '"aojsfoagonaogno"',
 '"qieng0qje"'
 ];
+
+
+
 var tip = $('.general-tip');
 tip.html(tips[Math.floor((Math.random() * tips.length))]);
 
 var gmail = $('#gmail').on('click', function() {
 	$('.gmail-tip').toggle();
 });
+
+
+$('.profile-photo').delay(500).animate({opacity:1},1000);
+$('.profile-name').delay(1500).animate({opacity:1},1000);
+$('.profile-subtitle').delay(2500).animate({opacity:1},1000);
+
 
 });
